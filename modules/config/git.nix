996058c892ba5a2ae0eb9fce13397a1dce5e7c06
thomas <twa.vermeulen@student.avans.nl>
2025-07-29 14:14:26 +0200

    move config in seperate files

---
+++ modules/config/git.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, lib, ... }:
+
+let
+  name = "thomas";
+  email = "twa.vermeulen@student.avans.nl";
+in
+{
+  programs.git = {
+    enable = true;
+    ignores = [ "*.swp" ];
+    userName = name;
+    userEmail = email;
+    lfs = {
+      enable = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+        editor = "vim";
+        autocrlf = "input";
+      };
+      pull.rebase = true;
+      rebase.autoStash = true;
+    };
+  };
+}
---
+++ modules/config/kitty.nix
@@ -0,0 +1,11 @@
+{ pkgs, config, lib, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+  };
+}
